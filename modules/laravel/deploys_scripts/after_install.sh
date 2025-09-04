be3645abfc38b9866a8c4da3a1a637a5193a6d06
satoshi256kbyte <satoshi.kaneyasu@serverworks.co.jp>
2025-09-05 00:06:00 +0900

    Remove Composer installation commands from after_install.sh script

--- modules/laravel/deploys_scripts/after_install.sh
+++ modules/laravel/deploys_scripts/after_install.sh
@@ -5,11 +5,6 @@ chmod -R 777 /var/www/html/storage
 chmod -R 777 /var/www/html/bootstrap/cache
 echo "Create SQLite database(demo only)"
 cd /var/www/html/
-
-# Install Composer dependencies
-composer install --no-dev --optimize-autoloader
-composer update --no-dev
-
 touch database/database.sqlite
 chmod 664 database/database.sqlite
 chown apache:apache database/database.sqlite
